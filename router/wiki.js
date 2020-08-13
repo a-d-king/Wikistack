97d62efd2ef5017029d4a38310d99aeb9f19a74e
Kirby <kirbyc6@gmail.com>
2020-08-13 15:05:15 -0400

    first commit

---
+++ router/wiki.js
@@ -0,0 +1,23 @@
+const express = require(‘express’)
+const {addPage} = require ('../views')
+
+const router = express.Router()
+
+
+router.get(‘/’, (req, res, next) => {
+    res.send(‘got to GET /wiki/‘)
+});
+
+
+router.post(‘/’, (req, res, next) => {
+    res.send(‘got to GET /wiki/‘)
+});
+
+
+router.get(‘/add’, (req, res, next) => {
+    res.send(‘addPage()’)
+});
+
+
+
+module.exports = router;
